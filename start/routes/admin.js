083a39cf0ebca02d32bac68d3bd800419ffd6f91
Gabriel Bittencourt <gabriel.bc88@gmail.com>
2020-10-12 18:11:31 -0300

    middleware routes admin

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -4,10 +4,30 @@
 const Route = use('Route')
 
 Route.group(() => {
+  /**
+   * Rotas de Categorias
+   */
   Route.resource('categories', 'CategoryController').apiOnly()
+  /**
+   * Rotas de Produtos
+   */
   Route.resource('products', 'ProductController').apiOnly()
+  /**
+   * Rotas de Cupons
+   */
   Route.resource('coupons', 'CouponController').apiOnly()
+  /**
+   * Rotas de Pedidos
+   */
+  Route.post('orders/:id/discount', 'OrderController.applyDiscount')
+  Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
   Route.resource('orders', 'OrderController').apiOnly()
+  /**
+   * Rotas de Imagens
+   */
   Route.resource('images', 'ImageController').apiOnly()
+  /**
+   * Rotas de Usuários
+   */
   Route.resource('users', 'UserController').apiOnly()
-}).prefix('v1/admin').namespace('Admin')
+}).prefix('v1/admin').namespace('Admin').middleware(['auth', 'is:(admin || manager)'])
